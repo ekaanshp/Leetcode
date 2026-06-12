07496f45de6ffebf47948fdea1594aa17b79f1b9
Ekaansh Patel <egpatel24@gmail.com>
2026-06-11 21:57:07 -0400

    Time: 0 ms (100.00%) | Memory: 13.3 MB (75.52%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,26 +1,26 @@
 class Solution {
     std::vector<string> result;
     string parenthesis = "";
-    void backtrack(int openN, int closedN, int n, std::vector<string>& result, string& parenthesis){
+    void backtrack(int openN, int closedN, int n){
         if(openN == closedN && closedN == n){
             result.push_back(parenthesis);
         }
 
         if(openN < n){
             parenthesis += '(';
-            backtrack(openN + 1, closedN, n, result, parenthesis);
-            parenthesis.pop_back();
+            backtrack(openN + 1, closedN, n);
+            parenthesis.pop_back(); 
         }
 
         if(closedN < openN){
             parenthesis += ')';
-            backtrack(openN, closedN + 1, n, result, parenthesis);
+            backtrack(openN, closedN + 1, n);
             parenthesis.pop_back();
         }
     }
 public:
     vector<string> generateParenthesis(int n) {
-        backtrack(0, 0, n, result, parenthesis);
+        backtrack(0, 0, n);
         return result;
     }
 };
