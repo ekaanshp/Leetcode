d932f1472615c66420b1bd625dc4d7577d8301cc
Ekaansh Patel <egpatel24@gmail.com>
2026-06-14 15:45:04 -0400

    Time: 2 ms (72.52%) | Memory: 10.5 MB (40.87%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,37 +1,42 @@
 class Solution {
     int n;
     std::vector<std::vector<string>> result;
-    std::unordered_set<int> column;
-    std::unordered_set<int> pos_diag;
-    std::unordered_set<int> neg_diag;
-    std::vector<string> board;
+    std::vector<bool> column;
+    std::vector<bool> pos_diag;
+    std::vector<bool> neg_diag;
+    
+    std::vector<std::string> board;
 
-    void backtrack(int r){
-        if(r == n){
+    void backtrack(int r) {
+        if (r == n) {
             result.push_back(board);
             return;
         }
 
-        for(int c = 0; c < n; c++){
-            if(column.contains(c) || pos_diag.contains(r + c) || neg_diag.contains(r - c)){
+        for (int c = 0; c < n; c++) {
+            if (column[c] || pos_diag[r + c] || neg_diag[r - c + n]) {
                 continue;
             }
-            column.insert(c);
-            pos_diag.insert(r + c);
-            neg_diag.insert(r - c);
+
+            column[c] = true;
+            pos_diag[r + c] = true;
+            neg_diag[r - c + n] = true;
             board[r][c] = 'Q';
 
             backtrack(r + 1);
 
-            column.erase(c);
-            pos_diag.erase(r + c);
-            neg_diag.erase(r - c);
+            column[c] = false;
+            pos_diag[r + c] = false;
+            neg_diag[r - c + n] = false;
             board[r][c] = '.';
         }
     }
 public:
     vector<vector<string>> solveNQueens(int n) {
         this->n = n;
+        column.resize(n, false);
+        pos_diag.resize(2 * n, false);
+        neg_diag.resize(2 * n, false);
         board = std::vector<std::string>(n, std::string(n, '.'));
         backtrack(0);
         return result;
