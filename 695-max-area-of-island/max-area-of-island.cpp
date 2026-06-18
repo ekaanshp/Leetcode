74048bead19df52abd051fd6a876bb1226dc1122
Ekaansh Patel <egpatel24@gmail.com>
2026-06-17 20:53:13 -0400

    Time: 3 ms (53.24%) | Memory: 28.3 MB (53.41%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -3,28 +3,26 @@ public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        std::vector<std::vector<bool>> islands(rows, std::vector<bool>(cols, false));
         int maxArea = 0;
 
-        std::function<int(int, int, std::vector<std::vector<bool>>&)> dfs =
-        [&](int r, int c, std::vector<std::vector<bool>>& visited){
-            if(r < 0 || r >= rows || c < 0 || c >= cols || visited[r][c] || grid[r][c] != 1){
+        std::function<int(int, int)> dfs = [&](int r, int c) {
+            if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] != 1) {
                 return 0;
             }
 
-            visited[r][c] = true;
+            grid[r][c] = 0;
 
-            return 1 + dfs(r + 1, c, visited) +
-            dfs(r - 1, c, visited) +
-            dfs(r, c + 1, visited) +
-            dfs(r, c - 1, visited);
+            return 1 + dfs(r + 1, c)
+                     + dfs(r - 1, c)
+                     + dfs(r, c + 1)
+                     + dfs(r, c - 1);
         };
 
-        for(int r = 0; r < rows; r++){
-            for(int c = 0; c < cols; c++){
-                if(grid[r][c] == 1 && !islands[r][c]){
-                    int currentIslandArea = dfs(r, c, islands);
-                    if(currentIslandArea > maxArea){
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    int currentIslandArea = dfs(r, c);
+                    if (currentIslandArea > maxArea) {
                         maxArea = currentIslandArea;
                     }
                 }
