013c5551b70c5611870def3cfa9ecf9c1fdaeba9
Ekaansh Patel <egpatel24@gmail.com>
2026-06-17 20:08:12 -0400

    Time: 4 ms (78.23%) | Memory: 23.2 MB (51.29%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -3,38 +3,43 @@ public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int rows = heights.size();
         int cols = heights[0].size();
-        std::set<std::pair<int, int>> pacificReachable;
-        std::set<std::pair<int, int>> atlanticReachable;
+        vector<vector<bool>> pacific(rows, vector<bool>(cols, false));
+        vector<vector<bool>> atlantic(rows, vector<bool>(cols, false));
 
-        std::function<void(int, int, std::set<std::pair<int, int>>&, int)> dfs = 
-        [&](int r, int c, std::set<std::pair<int, int>>& visit, int prevHeight){
-            if(visit.contains({r, c}) || r < 0 || r >= rows || c < 0 || c >= cols || heights[r][c] < prevHeight){
+        std::function<void(int, int, vector<vector<bool>>&, int)> dfs = 
+        [&](int r, int c, vector<vector<bool>>& visit, int prevHeight) {
+            if (r < 0 || r >= rows || c < 0 || c >= cols || 
+                visit[r][c] || heights[r][c] < prevHeight) {
                 return;
             }
 
-            visit.insert({r, c});
+            visit[r][c] = true;
+            
             dfs(r + 1, c, visit, heights[r][c]);
             dfs(r - 1, c, visit, heights[r][c]);
             dfs(r, c + 1, visit, heights[r][c]);
             dfs(r, c - 1, visit, heights[r][c]);
         };
 
-        for(int c = 0; c < cols; c++){
-            dfs(0, c, pacificReachable, heights[0][c]);
-            dfs(rows - 1, c, atlanticReachable, heights[rows - 1][c]);
+        for (int c = 0; c < cols; c++) {
+            dfs(0, c, pacific, heights[0][c]);
+            dfs(rows - 1, c, atlantic, heights[rows - 1][c]);
         }
 
-        for(int r = 0; r < rows; r++){
-            dfs(r, 0, pacificReachable, heights[r][0]);
-            dfs(r, cols - 1, atlanticReachable, heights[r][cols - 1]);
+        for (int r = 0; r < rows; r++) {
+            dfs(r, 0, pacific, heights[r][0]);
+            dfs(r, cols - 1, atlantic, heights[r][cols - 1]);
         }
 
-        std::vector<std::vector<int>> result;
-        for(auto cell : pacificReachable){
-            if(atlanticReachable.contains(cell)){
-                result.push_back({cell.first, cell.second});
+        vector<vector<int>> result;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (pacific[r][c] && atlantic[r][c]) {
+                    result.push_back({r, c});
+                }
             }
         }
+        
         return result;
     }
 };
